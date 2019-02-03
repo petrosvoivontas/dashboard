a73eb692948ed0c09c5d87aa396c91866655107a
petrosvoivontas <petervoivontas@outlook.com>
2019-02-03 19:43:15 +0200

    'sdfsdf'

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import $ from 'jquery';
+import React, { Component } from 'react';
+import './App.css';
+const {url} = require('./config');
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password:''
+    }
+    this.handleEmailSignup = this.handleEmailSignup.bind(this);
+    this.handleEmailLogin = this.handleEmailLogin.bind(this);
+  }
+
+  handleEmailSignup () {
+    const email = this.state.email;
+    const password = this.state.password;
+    const responseDisplay = $('.response');
+    fetch(`${url}/users/signup?email=${email}&password=${password}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Request failed');
+      }, networkError => console.log(networkError))
+      .then(jsonResponse => {
+        responseDisplay.text(jsonResponse);
+      })
+  }
+
+  handleEmailLogin () {
+    const email = this.state.email;
+    const password = this.state.password;
+    const responseDisplay = $('.response');
+    fetch(`${url}/users/login?email=${email}&password=${password}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Request faild.');
+      }, networkError => console.log(networkError))
+      .then(jsonResponse => {
+        responseDisplay.text(jsonResponse);
+      })
+  }
+
+  handleGoogleSignup () {
+    const responseDisplay = $('.response');
+    fetch(`${url}/users/google`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+      }, networkError => console.log(networkError))
+      .then(jsonResponse => {
+        responseDisplay.text(jsonResponse)
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <input 
+          className='email'
+          type='text'
+          value={this.state.email}
+          onChange={event => this.setState({email: event.target.value})}>
+        </input>
+        <input
+          className='password'
+          type='text'
+          value={this.state.password}
+          onChange={event => this.setState({password: event.target.value})}>
+        </input>
+        <button onClick={this.handleEmailSignup}>Sign up</button>
+        <button onClick={this.handleEmailLogin}>Log in</button>
+        <button onClick={this.handleGoogleSignup}>Google</button>
+        <p className='response'></p>
+      </div>
+    )
+  }
+}
+
+export default App;
